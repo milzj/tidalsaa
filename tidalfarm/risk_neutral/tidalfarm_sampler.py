322aa177ab1b9e11693a5d9bfa0c2208e45bcd57
Johannes Milz <milz@ma.tum.de>
2023-05-20 20:51:16 -0400

    Fixed sampler.

--- tidalfarm/risk_neutral/tidalfarm_sampler.py
+++ tidalfarm/risk_neutral/tidalfarm_sampler.py
@@ -13,7 +13,8 @@ class TidalfarmSampler(Sampler):
         q = sampler.random_base2(m=m)
         q = q + 1/(2*2**m) # shift
 
-        samples = truncnorm.ppf(q, a/std, b/std, loc=loc, scale=std)
+        a, b = (a - loc) / std, (b - loc) / std
+        samples = truncnorm.ppf(q, a, b, loc=loc, scale=std)
         self._samples = samples
 
 
@@ -23,9 +24,32 @@ class TidalfarmSampler(Sampler):
 
 if __name__ == "__main__":
 
-    loc = 0.005
-    sampler = TidalfarmSampler(m=3, loc=loc, std=5.0*loc)
-    print(sampler._samples)
+    import matplotlib.pyplot as plt
+
+    import os
+    path = 'truncated_normal'
+    os.makedirs(path, exist_ok=True)
+
+
+    loc = 0.0025
+    a = 0.0
+    b = 1.0
+
+    std = 0.01
+    m = 20
+    sampler = TidalfarmSampler(m=m, a=a, b=b, loc=loc, std=std)
+    print(min(sampler._samples))
+    print(max(sampler._samples))
     print("------------------")
     print(sampler._samples[0])
     print(sampler.sample(0))
+
+
+    fig, ax = plt.subplots(1, 1)
+    r = sampler._samples
+    nbins = 50
+    ax.hist(r, density=True, histtype='stepfilled', alpha=0.2, bins=nbins)
+    ax.set_title("truncated normal variables")
+
+    fig.tight_layout()
+    plt.savefig(os.path.join(path, "hist_truncated_normal.png"))
